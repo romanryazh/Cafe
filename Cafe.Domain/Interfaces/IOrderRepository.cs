16cf26c1ef62d489627a1c8de396e29af68adbf9
Roman Ryazh <romanryazh@yandex.ru>
2025-09-13 05:17:28 +0300

    создал интерфейсы репозиториев

---
+++ Cafe.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cafe.Domain.Entities;
+using Cafe.Domain.Enums;
+
+namespace Cafe.Domain.Interfaces;
+
+public interface IOrderRepository : IRepository<Order>
+{
+    Task<IReadOnlyCollection<Order>>
+        GetByStatusAsync(OrderStatus status, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyCollection<Product>> GetByIdsAsync(IEnumerable<Guid> ids,
+        CancellationToken cancellationToken = default);
+}
---
+++ Cafe.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cafe.Domain.Entities;
+
+namespace Cafe.Domain.Interfaces;
+
+public interface IRepository<T> where T : EntityBase
+{
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    
+    Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+    
+    Task Update(T entity);
+    
+    Task Delete(T entity);
+}
